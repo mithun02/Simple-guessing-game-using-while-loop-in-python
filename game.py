4292ef91f79c814cd15d563cc7c4b492be3beb5d
Mithun <92502966+mithun02@users.noreply.github.com>
2023-08-06 09:09:12 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,21 @@
+secret_number=4
+
+guess_count=0
+
+guess_limit=3
+
+while guess_count<guess_limit:
+
+  guess=int(input('Guess:'))
+
+  guess count+=1
+
+  if guess secret_number:
+
+  print("You have Won!")
+
+  break
+
+  else:
+
+print(" Sorry You have failed!")
